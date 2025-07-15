2b5250e926fd14ddc4a589e0820c13d473a18481
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-15 11:14:28 +0530

    add event and user models

---
+++ models/event.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  dateTime: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    address: String,
+    city: String,
+    state: String,
+    country: String,
+  },
+  capacity: {
+    type: Number,
+    max: 1000,
+    required: true,
+  },
+  registrations: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+export const Event =
+  mongoose.models.event || mongoose.model("Event", eventSchema);
